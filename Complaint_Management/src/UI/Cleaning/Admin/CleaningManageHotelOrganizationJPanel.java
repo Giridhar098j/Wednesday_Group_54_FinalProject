6d939032a2e0e979fb876365de98e57da1c4b06d
atharvagadgil13 <gadgil.at@northeastern.edu>
2024-12-08 15:38:03 -0500

    Added Functionality to Managing Organizations

--- Complaint_Management/src/UI/Cleaning/Admin/CleaningManageHotelOrganizationJPanel.java
+++ Complaint_Management/src/UI/Cleaning/Admin/CleaningManageHotelOrganizationJPanel.java
@@ -4,17 +4,56 @@
  */
 package UI.Cleaning.Admin;
 
+import Business.Organization.HotelOrganization;
+import Business.Organization.HotelOrganization.Type;
+import Business.Organization.HotelOrganizationDirectory;
+import java.awt.CardLayout;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Atharva
  */
 public class CleaningManageHotelOrganizationJPanel extends javax.swing.JPanel {
+    
+     private HotelOrganizationDirectory directory;
+    private JPanel userProcessContainer;
 
     /**
      * Creates new form CleaningManageHotelOrganizationJPanel
      */
-    public CleaningManageHotelOrganizationJPanel() {
+    public CleaningManageHotelOrganizationJPanel(JPanel userProcessContainer,HotelOrganizationDirectory directory) {
         initComponents();
+        this.userProcessContainer = userProcessContainer;
+        this.directory = directory;
+        
+        populateTable();
+        populateCombo();
+    }
+    
+     private void populateCombo(){
+        
+        orgnizationJComboBox.addItem(Type.Maintainance);
+                orgnizationJComboBox.addItem(Type.HouseKeeping);
+
+        
+        
+    }
+
+    private void populateTable(){
+        DefaultTableModel model = (DefaultTableModel) OrganizationsJTable.getModel();
+        
+        model.setRowCount(0);
+        
+        for (HotelOrganization organization : directory.getHotelOrganizationList()){
+            Object[] row = new Object[1];
+         
+            row[0] = organization.getName();
+            
+            model.addRow(row);
+        }
     }
 
     /**
@@ -29,7 +68,7 @@ public class CleaningManageHotelOrganizationJPanel extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         btnBack = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        OrganizationsJTable = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         orgnizationJComboBox = new javax.swing.JComboBox<>();
         btnAddOrganization = new javax.swing.JButton();
@@ -38,8 +77,13 @@ public class CleaningManageHotelOrganizationJPanel extends javax.swing.JPanel {
         jLabel1.setText("Cleaning Managing Organizations");
 
         btnBack.setText("Back<<");
+        btnBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackActionPerformed(evt);
+            }
+        });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        OrganizationsJTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null},
                 {null},
@@ -50,13 +94,18 @@ public class CleaningManageHotelOrganizationJPanel extends javax.swing.JPanel {
                 "Name"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(OrganizationsJTable);
 
         jLabel2.setText("Organization Type :");
 
         orgnizationJComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         btnAddOrganization.setText("Add Organization");
+        btnAddOrganization.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddOrganizationActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -99,14 +148,34 @@ public class CleaningManageHotelOrganizationJPanel extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnAddOrganizationActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddOrganizationActionPerformed
+        // TODO add your handling code here:
+        Type type = (Type) orgnizationJComboBox.getSelectedItem();
+       
+        directory.createHotelOrganization(type);
+        JOptionPane.showMessageDialog(null,"Organization has been added successfully!");
+       
+        
+        
+            
+        populateTable();
+    }//GEN-LAST:event_btnAddOrganizationActionPerformed
+
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+        // TODO add your handling code here:
+         userProcessContainer.remove(this);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.previous(userProcessContainer);
+    }//GEN-LAST:event_btnBackActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable OrganizationsJTable;
     private javax.swing.JButton btnAddOrganization;
     private javax.swing.JButton btnBack;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JComboBox<String> orgnizationJComboBox;
     // End of variables declaration//GEN-END:variables
 }
