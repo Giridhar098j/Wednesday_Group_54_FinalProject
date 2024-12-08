1af6f00120f326c0d1a772edbf4cb414b98cb16d
atharvagadgil13 <gadgil.at@northeastern.edu>
2024-12-08 18:04:00 -0500

    Added 4 more Roles

---
+++ Complaint_Management/src/Business/Role/HotelAdminRole.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSys;
+import Business.HotelEnterprise.HotelEnterprise;
+import Business.Network.HotelNetwork;
+import Business.Organization.HotelOrganization;
+import Business.UserAccount.UserAccount;
+import UI.HotelManagement.HotelAdminRole.HotelManagementHotelAdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Atharva
+ */
+public class HotelAdminRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, HotelOrganization organization, HotelEnterprise enterprise, EcoSys business,HotelNetwork network) {
+        return new HotelManagementHotelAdminWorkAreaJPanel(userProcessContainer, enterprise,network);
+    }
+
+    
+}
